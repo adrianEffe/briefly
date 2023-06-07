4bfea1c2aaee77d51fe32d193b79903fbbafd9ec
Adrian <adrian.iosdev@gmail.com>
2023-06-07 16:57:58 +0200

    Create a routes mod

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use axum::extract::Path;
 use axum::routing::get;
 use axum::Router;
+use briefly::routes::{health_check};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/shorten:user", get(shorten));
+    let app = Router::new()
+        .route("/health_check", get(health_check))
+        .route("/shorten/:user", get(shorten));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
