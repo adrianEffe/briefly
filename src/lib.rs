d9caaf6a0de69df04ebf82f0cdec839749def911
Adrian <adrian.iosdev@gmail.com>
2023-06-11 18:54:32 +0300

    Move spawning app to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,15 @@
 pub mod routes;
+
+use axum::routing::get;
+use axum::Router;
+use routes::health_check;
+
+pub async fn run() {
+    let app = Router::new()
+        .route("/health_check", get(health_check));
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,6 @@
-use axum::extract::Path;
-use axum::routing::get;
-use axum::Router;
-use briefly::routes::{health_check};
+use briefly::run;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/health_check", get(health_check))
-        .route("/shorten/:user", get(shorten));
-
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap()
-}
-
-async fn shorten(Path(user): Path<String>) -> String {
-    format!("Hello World from {}", user)
+    run().await
 }
