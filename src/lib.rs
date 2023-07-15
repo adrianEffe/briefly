4b06d88775f6ae9d464a110c85bd3be0cf3ddbe1
Adrian <adrian.iosdev@gmail.com>
2023-07-15 11:45:35 +0300

    Create TestApp for testing purposes

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
 pub mod routes;
-
 use axum::routing::get;
 use axum::Router;
 use routes::health_check;
 
 pub async fn run() {
-    let app = Router::new()
-        .route("/health_check", get(health_check));
+    let app = app();
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap()
+}
+
+pub fn app() -> Router {
+    Router::new().route("/health_check", get(health_check))
 }
