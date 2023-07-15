ec994ef70e35de69ad31bfd6c47fac9123627b1c
Adrian <adrian.iosdev@gmail.com>
2023-07-15 12:13:39 +0300

    Assign random port to tests

--- Tests/health_check.rs
+++ Tests/health_check.rs
@@ -1,24 +1,25 @@
-use std::net::TcpListener;
 use axum::Server;
+use std::net::TcpListener;
 
 pub struct TestApp {
     pub base_url: String,
-    pub client: reqwest::Client
+    pub client: reqwest::Client,
 }
 
-//TODO: assign random port for tests
 impl TestApp {
     pub async fn new() -> TestApp {
-        let listener = TcpListener::bind("0.0.0.0:3000").expect("Could not bind ephemeral socket");
+        let listener = TcpListener::bind("127.0.0.1:0").expect("Could not bind ephemeral socket");
+        let port = listener.local_addr().unwrap().port();
 
         tokio::spawn(async move {
-            let server = Server::from_tcp(listener).unwrap().serve(briefly::app()
-                .into_make_service());
+            let server = Server::from_tcp(listener)
+                .unwrap()
+                .serve(briefly::app().into_make_service());
             server.await.expect("server error");
         });
 
         TestApp {
-            base_url: String::from("http://0.0.0.0:3000/"),
+            base_url: String::from(format!("http://127.0.0.1:{}/", port)),
             client: reqwest::Client::new(),
         }
     }
@@ -30,6 +31,15 @@ impl TestApp {
 
 #[tokio::test]
 async fn health_check_works() {
+    let app = TestApp::new().await;
+    let response = app.get("health_check").send().await.unwrap();
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn health_check_works_for_random_port() {
     let app = TestApp::new().await;
     let response = app.get("health_check").send().await.unwrap();
 
