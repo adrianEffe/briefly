10bc2c8ba3d1cd0f4ade6d4d8a28e5aea350dcdd
Adrian <adrian.iosdev@gmail.com>
2023-07-26 18:55:26 +0300

    Add db as app state

--- src/lib.rs
+++ src/lib.rs
@@ -5,19 +5,41 @@ use axum::routing::{get, post};
 use axum::Router;
 use configuration::get_configuration;
 use routes::{full_url, health_check};
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use std::sync::Arc;
 
 pub async fn run() {
-    let app = app();
     let configuration = get_configuration().expect("Failed to read configuration");
+
+    let pool = connect_to_database(configuration.database.connection_string())
+        .await
+        .expect("Failed to conect to the database");
+    let app_state = Arc::new(AppState { db: pool.clone() });
     let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let app = app(app_state);
+
     axum::Server::bind(&address.parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap()
 }
 
-pub fn app() -> Router {
+pub struct AppState {
+    db: Pool<Postgres>,
+}
+
+pub fn app(app_state: Arc<AppState>) -> Router {
     Router::new()
         .route("/health_check", get(health_check))
         .route("/full_url", post(full_url))
+        .with_state(app_state)
+}
+
+async fn connect_to_database(connection_string: String) -> Option<Pool<Postgres>> {
+    PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&connection_string)
+        .await
+        .ok()
 }
