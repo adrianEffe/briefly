01604d72f9faaabec77af601b81202ecc5422b34
Adrian <adrian.iosdev@gmail.com>
2023-07-27 11:24:49 +0300

    Update to sqlx reversable migrations

---
+++ migrations/migrations/20230726160018_init.up.sql
@@ -0,0 +1,9 @@
+-- Add up migration script here
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE briefly(
+    id UUID PRIMARY KEY NOT NULL DEFAULT (uuid_generate_v4()),
+    url TEXT NOT NULL,
+    extension TEXT NOT NULL UNIQUE,
+    created_at timestamp NOT NULL
+);
