0ff227bbada4fce8a6ae859e04cb52c7c557f91c
Adrian <adrian.iosdev@gmail.com>
2023-07-30 15:22:14 +0300

    Update run function to take tcp address and db connection string

--- src/lib.rs
+++ src/lib.rs
@@ -5,23 +5,20 @@ pub mod schema;
 
 use axum::routing::{get, post};
 use axum::Router;
-use configuration::get_configuration;
 use routes::{full_url, health_check};
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use std::net::TcpListener;
 use std::sync::Arc;
 
-pub async fn run() {
-    let configuration = get_configuration().expect("Failed to read configuration");
-
-    let pool = connect_to_database(configuration.database.connection_string())
+pub async fn run(listener: TcpListener, db_connection: &str) {
+    let pool = connect_to_database(db_connection)
         .await
         .expect("Failed to conect to the database");
     let app_state = Arc::new(AppState { db: pool.clone() });
-    let address = format!("127.0.0.1:{}", configuration.application_port);
 
     let app = app(app_state);
-
-    axum::Server::bind(&address.parse().unwrap())
+    axum::Server::from_tcp(listener)
+        .unwrap()
         .serve(app.into_make_service())
         .await
         .unwrap()
@@ -38,10 +35,10 @@ pub fn app(app_state: Arc<AppState>) -> Router {
         .with_state(app_state)
 }
 
-async fn connect_to_database(connection_string: String) -> Option<Pool<Postgres>> {
+async fn connect_to_database(connection_string: &str) -> Option<Pool<Postgres>> {
     PgPoolOptions::new()
         .max_connections(10)
-        .connect(&connection_string)
+        .connect(connection_string)
         .await
         .ok()
 }
