731187a449b8d8dc5628351e595c434aac04222d
Adrian <adrian.iosdev@gmail.com>
2023-08-06 17:39:34 +0300

    Add basic telemetry - tracing (#15)
    * Add basic telemetry - tracing
    
    * Update to tracing with requests id

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use briefly::{configuration::get_configuration, run};
 use std::net::TcpListener;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_string = configuration.database.connection_string();
     let address = format!("127.0.0.1:{}", configuration.application_port);
