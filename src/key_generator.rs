61ac2533d3a7abf30ea1ed8c64cff3e99658f2c2
Adrian <adrian.iosdev@gmail.com>
2023-08-13 18:29:38 +0300

    Use shortener generator for POST endpoint

--- src/key_generator.rs
+++ src/key_generator.rs
@@ -1,8 +1,8 @@
 use base62::encode;
 use md5::compute;
 
-pub fn generate(url: &str) -> String {
-    let hash = compute(url);
+pub fn generate(value: &str) -> String {
+    let hash = compute(value);
     let hash_bytes: [u8; 16] = hash.into();
     let hash_u128: u128 = u128::from_le_bytes(hash_bytes);
     encode(hash_u128)[..7].to_string()
--- src/routes/full_url.rs
+++ src/routes/full_url.rs
@@ -1,6 +1,9 @@
-use crate::{model::UrlRequestModel, schema::CreateShortUrlSchema, AppState};
+use crate::{
+    key_generator::generate, model::UrlRequestModel, schema::CreateShortUrlSchema, AppState,
+};
 use axum::{extract::State, Json};
 use chrono::Utc;
+use sqlx::Error;
 use std::sync::Arc;
 use uuid::Uuid;
 
@@ -8,23 +11,42 @@ pub async fn full_url(
     State(data): State<Arc<AppState>>,
     Json(payload): Json<CreateShortUrlSchema>,
 ) {
-    let query_result = sqlx::query_as!(
+    let mut retry_count = 3;
+
+    while retry_count > 0 {
+        let query_result = insert_in_db(&payload.url, State(data.clone())).await;
+
+        match query_result {
+            Ok(note) => {
+                println!("okay received testing: {:?}", note);
+                break;
+            }
+            Err(e) => {
+                println!("failed with error: {:?}", e);
+                //TODO : - For now assume is failing because of error code 23505, that stands for
+                //duplicate key
+                retry_count += 1;
+            }
+        }
+    }
+}
+
+async fn insert_in_db(
+    url: &str,
+    State(data): State<Arc<AppState>>,
+) -> Result<UrlRequestModel, Error> {
+    let uuid = Uuid::new_v4();
+    let modifier = uuid.to_string() + url;
+    let shortened = generate(&modifier);
+
+    sqlx::query_as!(
         UrlRequestModel,
         "INSERT INTO briefly (id, url, extension, created_at) VALUES ($1, $2, $3, $4) RETURNING *",
-        Uuid::new_v4(),
-        payload.url.to_string(),
-        Uuid::new_v4().to_string(), // TODO: - handle extension to be shortened url extension
+        uuid,
+        url.to_string(),
+        shortened,
         Utc::now()
     )
     .fetch_one(&data.db)
-    .await;
-
-    match query_result {
-        Ok(note) => {
-            println!("okay received testing: {:?}", note);
-        }
-        Err(e) => {
-            println!("{:?}", e);
-        }
-    }
+    .await
 }
