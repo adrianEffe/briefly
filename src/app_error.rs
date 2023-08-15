bf2df5755168f946235ed03519866d796003d68a
Adrian <adrian.iosdev@gmail.com>
2023-08-15 12:17:09 +0300

    Handle error for Post request

---
+++ src/app_error.rs
@@ -0,0 +1,16 @@
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
+
+pub struct AppError(pub anyhow::Error);
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", self.0),
+        )
+            .into_response()
+    }
+}
--- src/routes/full_url.rs
+++ src/routes/full_url.rs
@@ -1,6 +1,8 @@
 use crate::{
-    key_generator::generate, model::UrlRequestModel, schema::CreateShortUrlSchema, AppState,
+    app_error::AppError, key_generator::generate, model::UrlRequestModel,
+    schema::CreateShortUrlSchema, AppState,
 };
+use anyhow::anyhow;
 use axum::{extract::State, Json};
 use chrono::Utc;
 use sqlx::Error;
@@ -10,7 +12,7 @@ use uuid::Uuid;
 pub async fn full_url(
     State(data): State<Arc<AppState>>,
     Json(payload): Json<CreateShortUrlSchema>,
-) -> String {
+) -> Result<String, AppError> {
     let mut retry_count = 3;
 
     while retry_count > 0 {
@@ -18,22 +20,17 @@ pub async fn full_url(
 
         match query_result {
             Ok(request) => {
-                println!("okay received testing: {:?}", request);
-                return request.extension;
+                return Ok(request.extension);
             }
             Err(e) => {
-                println!("failed with error: {:?}", e);
-                //TODO : - For now assume is failing because of error code 23505, that stands for
-                //duplicate key
                 if retry_count == 1 {
-                    return "error".to_string();
+                    return Err(AppError(e.into()));
                 }
                 retry_count -= 1;
             }
         }
     }
-
-    "internal server error".to_string()
+    Err(AppError(anyhow!("Something went wrong")))
 }
 
 async fn insert_in_db(
