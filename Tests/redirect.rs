712651a0fad5ce1107c82340c63468bec6320e95
Adrian <adrian.iosdev@gmail.com>
2023-08-20 15:16:01 +0300

    Update tests for updated schema

--- Tests/redirect.rs
+++ Tests/redirect.rs
@@ -5,7 +5,6 @@ use chrono::Utc;
 use reqwest::header::LOCATION;
 use shared::test_app::TestApp;
 use sqlx::{Connection, PgConnection};
-use uuid::Uuid;
 
 #[tokio::test]
 async fn redirect_303_for_valid_redirection() {
@@ -16,23 +15,22 @@ async fn redirect_303_for_valid_redirection() {
         .expect("Failed to connect to Postgres");
 
     let url = "https:://google.com";
-    let uuid = Uuid::new_v4();
-    let modifier = uuid.to_string() + url;
+    let date = Utc::now();
+    let modifier = date.to_string() + url;
     let shortened = generate(&modifier);
 
     let url_model = sqlx::query_as!(
         UrlRequestModel,
-        "INSERT INTO briefly (id, url, extension, created_at) VALUES ($1, $2, $3, $4) RETURNING *",
-        uuid,
-        url.to_string(),
+        "INSERT INTO briefly (id, url, created_at) VALUES ($1, $2, $3) RETURNING *",
         shortened,
+        url.to_string(),
         Utc::now()
     )
     .fetch_one(&mut connection)
     .await
     .unwrap();
 
-    let response = app.get(&url_model.extension).send().await.unwrap();
+    let response = app.get(&url_model.id).send().await.unwrap();
     assert_eq!(303, response.status().as_u16());
 
     let location = response.headers().get(LOCATION).unwrap().to_str().unwrap();
