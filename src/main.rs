4687b6c83b157b8428a6e7a8baa9fd07bca08e90
Adrian <adrian.iosdev@gmail.com>
2023-08-27 15:29:40 +0300

    Modularize configuration for local and production environment

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,10 @@ async fn main() {
 
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_string = configuration.database.connection_string();
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address).expect("Could not bind to socket");
     run(listener, &connection_string).await
 }
