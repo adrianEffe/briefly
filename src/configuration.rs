f44f76cae0e2b9a9fef8cabd11c2686d196610c0
Adrian <adrian.iosdev@gmail.com>
2023-08-27 15:41:17 +0300

    Modularize configuration for local and production environment (#23)
    * Add sqlx offline query data
    
    * Modularize configuration for local and production environment

--- src/configuration.rs
+++ src/configuration.rs
@@ -3,7 +3,13 @@ use config::Config;
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
 }
 
 #[derive(serde::Deserialize)]
@@ -16,7 +22,15 @@ pub struct DatabaseSettings {
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let builder = Config::builder().add_source(config::File::with_name("configuration"));
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT");
+    let filename = format!("configuration/{}", environment.as_str());
+    let builder = Config::builder()
+        .add_source(config::File::with_name("configuration/base"))
+        .add_source(config::File::with_name(&filename));
+
     let config = builder.build()?;
     config.try_deserialize()
 }
@@ -27,5 +41,33 @@ impl DatabaseSettings {
             "postgres://{}:{}@{}:{}/{}",
             self.username, self.password, self.host, self.port, self.database_name
         )
+    }
+}
+
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either `local` or `production`.",
+                other
+            )),
+        }
     }
 }
